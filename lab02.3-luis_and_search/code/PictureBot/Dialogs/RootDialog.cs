083ca5d2b2d7edeb43bc2e83cd3c88638f47820b
Anna Thomas <antho@microsoft.com>
2017-11-02 08:05:35 -0700

    thursday am checkin

---
+++ lab02.3-luis_and_search/code/PictureBot/Dialogs/RootDialog.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Scorables;
+using Microsoft.Bot.Connector;
+
+namespace PictureBot.Dialogs
+{
+    [LuisModel("TODO-modelID", "TODO-luisKey")]
+    [Serializable]
+    public class RootDialog : DispatchDialog
+    {
+        [RegexPattern("^hello")]
+        [RegexPattern("^hi")]
+        [ScorableGroup(0)]
+        public async Task Hello(IDialogContext context, IActivity activity)
+        {
+            await context.PostAsync("Hello from RegEx!  I am a Photo Organization Bot.  I can search your photos, share your photos on Twitter, and order prints of your photos.  You can ask me things like 'find pictures of food'.");
+        }
+
+        [RegexPattern("^help")]
+        [ScorableGroup(0)]
+        public async Task Help(IDialogContext context, IActivity activity)
+        {
+            // Launch help dialog with button menu  
+            List<string> choices = new List<string>(new string[] { "Search Pictures", "Share Picture", "Order Prints" });
+            PromptDialog.Choice<string>(context, ResumeAfterChoice, 
+                new PromptOptions<string>("How can I help you?", options:choices));
+        }
+
+        private async Task ResumeAfterChoice(IDialogContext context, IAwaitable<string> result)
+        {
+            string choice = await result;
+            
+            switch (choice)
+            {
+                case "Search Pictures":
+                    PromptDialog.Text(context, ResumeAfterSearchTopicClarification,
+                        "What kind of picture do you want to search for?");
+                    break;
+                case "Share Picture":
+                    await SharePic(context, null);
+                    break;
+                case "Order Prints":
+                    await OrderPic(context, null);
+                    break;
+                default:
+                    await context.PostAsync("I'm sorry. I didn't understand you.");
+                    break;
+            }
+        }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        [ScorableGroup(1)]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            // Luis returned with "None" as the winning intent,
+            // so drop down to next level of ScorableGroups.  
+            ContinueWithNextGroup();
+        }
+
+        [LuisIntent("Greeting")]
+        [ScorableGroup(1)]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            // Duplicate logic, for a teachable moment on Scorables.  
+            await context.PostAsync("Hello from LUIS!  I am a Photo Organization Bot.  I can search your photos, share your photos on Twitter, and order prints of your photos.  You can ask me things like 'find pictures of food'.");
+        }
+
+        [LuisIntent("SearchPics")]
+        [ScorableGroup(1)]
+        public async Task SearchPics(IDialogContext context, LuisResult result)
+        {
+            // Check if LUIS has identified the search term that we should look for.  
+            string facet = null;
+            EntityRecommendation rec;
+            if (result.TryFindEntity("facet", out rec)) facet = rec.Entity;
+
+            // If we don't know what to search for (for example, the user said
+            // "find pictures" or "search" instead of "find pictures of x"),
+            // then prompt for a search term.  
+            if (string.IsNullOrEmpty(facet))
+            {
+                PromptDialog.Text(context, ResumeAfterSearchTopicClarification,
+                    "What kind of picture do you want to search for?");
+            }
+            else
+            {
+                await context.PostAsync("Searching pictures...");
+                context.Call(new SearchDialog(facet), ResumeAfterSearchDialog);
+            }
+        }
+
+        private async Task ResumeAfterSearchTopicClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            string searchTerm = await result;
+            context.Call(new SearchDialog(searchTerm), ResumeAfterSearchDialog);
+        }
+
+        private async Task ResumeAfterSearchDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync("Done searching pictures");
+        }
+
+        [LuisIntent("OrderPic")]
+        [ScorableGroup(1)]
+        public async Task OrderPic(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Ordering your pictures...");
+        }
+
+        [LuisIntent("SharePic")]
+        [ScorableGroup(1)]
+        public async Task SharePic(IDialogContext context, LuisResult result)
+        {
+            PromptDialog.Confirm(context, AfterShareAsync,
+                "Are you sure you want to tweet this picture?");            
+        }
+
+        private async Task AfterShareAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            // This is a different way to get an awaiter!  Which do you like better?  
+            if (result.GetAwaiter().GetResult() == true)
+            {
+                // Yes, share the picture.
+                // NOTE: for purposes of this hands-on lab, we are not going to bother to get everyone set
+                // up with Twitter dev accounts and actually post, but feel free to implement if you want!
+                await context.PostAsync("Posting tweet.");
+            }
+            else
+            {
+                // No, don't share the picture.  
+                await context.PostAsync("OK, I won't share it.");
+            }
+        }
+
+        // Since none of the scorables in previous group won, the dialog sends a help message.
+        [MethodBind]
+        [ScorableGroup(2)]
+        public async Task Default(IDialogContext context, IActivity activity)
+        {
+            await context.PostAsync("I'm sorry. I didn't understand you.");
+            await context.PostAsync("You can tell me to find photos, tweet them, and order prints.  Here is an example: \"find pictures of food\".  Or say \"help\" for a lovely button menu!");
+        }
+
+    }
+}
