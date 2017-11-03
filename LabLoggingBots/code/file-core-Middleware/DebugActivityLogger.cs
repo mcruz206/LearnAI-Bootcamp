03c6354910ffbdc551450334789bdb6d558aadec
Mithun <mithun-prasad@users.noreply.github.com>
2017-11-03 09:06:28 -0700

    added testing labs

---
+++ LabLoggingBots/code/file-core-Middleware/DebugActivityLogger.cs
@@ -0,0 +1,24 @@
+﻿namespace MiddlewareBot
+{
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.History;
+    using Microsoft.Bot.Connector;
+    using System.IO;
+
+    public class DebugActivityLogger : IActivityLogger
+    {
+        TextWriter tw;
+        public DebugActivityLogger(TextWriter inputFile)
+        {
+            this.tw = inputFile;
+        }
+
+        public async Task LogAsync(IActivity activiy)
+        {
+            Debug.WriteLine($"From:{activiy.From.Id} - To:{activiy.Recipient.Id} - Message:{activiy.AsMessageActivity().Text}");
+            tw.WriteLine($"From:{activiy.From.Id} - To:{activiy.Recipient.Id} - Message:{activiy.AsMessageActivity().Text}", true);
+            tw.Flush();
+        }
+    }
+}
---
+++ LabLoggingBots/code/file-core-Middleware/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿namespace MiddlewareBot
+{
+    using System.Web.Http;
+    using Autofac;
+    using Microsoft.Bot.Builder.Dialogs;
+    using System.IO;
+    using System.Diagnostics;
+    using System;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        static TextWriter tw = null;
+        protected void Application_Start()
+        {
+            tw = new StreamWriter("C:\\Users\\miprasad\\Downloads\\log.txt", true);
+            Conversation.UpdateContainer(builder =>
+            {
+                builder.RegisterType<DebugActivityLogger>().AsImplementedInterfaces().InstancePerDependency().WithParameter("inputFile", tw);
+            });
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        protected void Application_End()
+        {
+            tw.Close();
+        }
+    }
+}
